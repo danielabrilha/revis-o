8da38c9a3f97954253b4a465a95ccefab342a067
975938 <975938@PITLAB2178799.senac.educ>
2024-02-06 07:52:12 -0300

    Revisão de exerecicio

---
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author 975938
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //exercicio 1
+        System.out.println("hello whord!");
+        
+        
+        // comentario 1
+        /*comentario 2*/
+        
+        // exercicio 3
+         String carname  = "valvo";
+         
+         // exercicio 4
+         int maxSpeed = 120;
+         
+         // exercicio 5
+         int x = 5;
+         int y = 10;
+         System.out.println(x + y);
+         
+         //exercicio 6
+         
+         x = 5;
+         int y = 10;
+         int z = x + y;
+         System.out.println(z);
+         
+         // exercicio 7
+         int x = 5, y = 6, z =50;
+         System.out.println(x + y + z);
+         
+         // exercicio 8
+         int myNum = 9;
+         float myFloatNum = 8.99f;
+         char myletter = 'a';
+         boolean myBOOL = false;
+         break myText = "hello word";
+    
+         // exercicio 9
+         // primitive data types.
+                  
+         // exercicio 10
+          double myDouble = 9.78d;
+          int myInt = Integer.parseInt(myDouble); 
+          
+          //exercicio 11
+          System.out.print(10*5);
+          
+        //exercicio 12
+          System.out.println(10/5);
+         
+         //exercicio 13
+          x = 10;
+         ++x;
+         
+         //exercicio 14
+          x = 10;
+         x +=5;
+         
+         //exercicio 15
+         String greeting = "hello";
+         
+         // exercicio 16
+         
+         
+          
+    }
+    
+}
